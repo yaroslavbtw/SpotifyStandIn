48349c7d85a817d75895af949f7ac32afb5fe651
yaroslav <yaroslavantonov47@gmail.com>
2023-07-12 04:50:08 +0300

    some changes according to sonarcloud and adding str methods to models objects

--- Music/models.py
+++ Music/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 class Genre(models.Model):
     title = models.CharField(max_length=100, null=False)
 
+    def __str__(self):
+        return self.title
+
 
 class Album(models.Model):
     ALBUM_TYPE_CHOICES = (
@@ -18,12 +21,18 @@ class Album(models.Model):
     release_date = models.DateField(auto_now_add=True)
     album_type = models.CharField(max_length=10, choices=ALBUM_TYPE_CHOICES, default='album')
 
+    def __str__(self):
+        return self.title + " - " + self.artist.name
+
 
 class Artist(models.Model):
     name = models.CharField(max_length=100, null=False)
     photo = models.ImageField(upload_to='data/artists_photo/', blank=True, null=True)
     verified = models.BooleanField(default=False, null=False)
     followers = models.PositiveBigIntegerField(default=0, blank=True, null=False)
+
+    def __str__(self):
+        return self.name
 
 
 class Song(models.Model):
@@ -38,15 +47,24 @@ class Song(models.Model):
     dt_create = models.DateField(auto_now_add=True, editable=False)
     listening = models.PositiveBigIntegerField(default=0, null=False, blank=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Playlist(models.Model):
     title = models.CharField(max_length=200, null=False)
     cover = models.ImageField(upload_to='data/playlist_covers/', blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
-    description = models.CharField(max_length=255, null=True, blank=True)
+    description = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title + " - " + self.user.username
 
 
 class SongPlaylist(models.Model):
     song = models.ForeignKey('Song', on_delete=models.DO_NOTHING, null=False)
     playlist = models.ForeignKey('Playlist', on_delete=models.DO_NOTHING, null=False)
     added_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.playlist.title + " - " + self.song.title + " - " + self.playlist.user.username
--- Users/urls.py
+++ Users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import *
+from .views import UserViewSet
 
 router = routers.DefaultRouter()
 router.register(r'', UserViewSet)
